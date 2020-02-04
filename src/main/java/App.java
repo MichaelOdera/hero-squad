f251b392ca31a961a9a61c8b1dee81160ffd6f14
MichaelOdera <michogelira@gmail.com>
2020-02-04 08:11:31 +0300

    created route for heros/new in App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,6 @@
 import com.github.jknack.handlebars.Handlebars;
 import models.Hero;
+import models.Squad;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
@@ -25,7 +26,8 @@ public class App{
             String power = request.queryParams("power");
             String weakness = request.queryParams("weakness");
             Integer age = Integer.parseInt(request.queryParams("age"));
-            Hero newHero = new Hero(name, age, power, weakness);
+            String squadName = request.queryParams("squad");
+            Hero newHero = new Hero(name, age, power, weakness, squadName);
             return new ModelAndView(model, "success.hbs");
         }, new HandlebarsTemplateEngine());
 
@@ -35,5 +37,40 @@ public class App{
             model.put("heros", heros);
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
+
+        get("heros/:id", (request, response)->{
+            Map<String, Object> model = new HashMap<>();
+            int idOfHeroToFind = Integer.parseInt(request.params("id"));
+            Hero lookedForHero = Hero.findById(idOfHeroToFind);
+            model.put("lookedForHero", lookedForHero);
+            return new ModelAndView(model, "hero-detail.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //create a function to display edit form on request
+
+        get("/heros/:id/update", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            int idOfHeroToEdit = Integer.parseInt(request.params("id"));
+            Hero editHero = Hero.findById(idOfHeroToEdit);
+            model.put("editHero", editHero);
+            return new ModelAndView(model, "edit-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+        //create a function to update a hero after edit details are submitted
+        post("/heros/:id/update", (request, response)->{
+            Map<String, Object> model = new HashMap<>();
+            int idOfHeroToEdit = Integer.parseInt(request.params("id"));
+            Hero editHero = Hero.findById(idOfHeroToEdit);
+            String name = request.queryParams("name");
+            String power = request.queryParams("power");
+            String weakness = request.queryParams("weakness");
+            Integer age = Integer.parseInt(request.queryParams("age"));
+            String squad = request.queryParams("squad");
+            editHero.update(name, age, power, weakness, squad);
+            return new ModelAndView(model, "edit-success.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
     }
 }
