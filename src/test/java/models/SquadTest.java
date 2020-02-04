2e08ec717c781ed650744c00affde2b08d7e4210
MichaelOdera <michogelira@gmail.com>
2020-02-04 08:07:59 +0300

    generate AquadTest.java

--- src/test/java/models/SquadTest.java
+++ src/test/java/models/SquadTest.java
@@ -2,6 +2,7 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +14,41 @@ public class SquadTest {
 
     @After
     public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testIfIsInstanceOfHero(){
+        Hero testHero = new Hero("thirty", 23, "slim", "travel", "hurry");
+        Squad testSquad = new Squad(testHero);
+        assertEquals(true, testSquad instanceof Squad);
+    }
+
+    @Test
+    public void testIfGetsproperties(){
+        Hero testHero = new Hero("thirty", 23, "slim", "travel", "hurry");
+        Squad testSquad = new Squad(testHero);
+        assertEquals(testHero, testSquad.getHero());
+    }
+
+    @Test
+    public void checkIfReturnsAge_23(){
+        Hero testHero = new Hero("thirty", 23, "slim", "travel", "hurry");
+        Squad testSquad = new Squad(testHero);
+        assertEquals(23, testSquad.getAge());
+    }
+
+    @Test
+    public void checkIfCapturesMagnitude_4(){
+        Hero testHero = new Hero("thirty", 23, "slim", "travel", "hurry");
+        Squad testSquad = new Squad(testHero);
+        assertEquals(4, testSquad.getPowerMagnitude());
+
+    }
+
+    @Test
+    public void checkIfReturnsNameOfSquad_hurry(){
+        Hero testHero = new Hero("thirty", 23, "slim", "travel", "hurry");
+        Squad testSquad = new Squad(testHero);
+        assertEquals("hurry", testSquad.getSquadName());
     }
 }
