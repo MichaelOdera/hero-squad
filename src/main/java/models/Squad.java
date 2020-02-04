3c60c9ac04c2bff8ba6eba79744afc01d3d973af
MichaelOdera <michogelira@gmail.com>
2020-02-04 08:13:58 +0300

    created a constructor and passed in Hero as a parameter

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -1,5 +1,47 @@
 package models;
 
-public class Squad {
+import java.util.ArrayList;
 
+public class Squad {
+    private Hero hero;
+    private static ArrayList<Squad> instances = new ArrayList<>();
+
+
+
+    public Squad(Hero hero){
+        this.hero = hero;
+        instances.add(this);
+
+    }
+
+
+    //Creating setters and getters for hero squad assignments
+    public void setHero(Hero hero) {
+        this.hero = hero;
+    }
+
+    public Hero getHero() {
+        return hero;
+
+    }
+    public static ArrayList<Squad> getAllInstances() {
+        return instances;
+    }
+
+    public static void setInstances(ArrayList<Squad> instances) {
+        Squad.instances = instances;
+    }
+
+
+    public int getAge() {
+        return hero.getAge();
+    }
+
+    public int getPowerMagnitude() {
+        return hero.getPowerMagnitude();
+    }
+
+    public String getSquadName() {
+        return hero.getSquadName();
+    }
 }
